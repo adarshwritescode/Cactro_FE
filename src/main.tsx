2f48fe73daca43032edbd92376047d3f96f9e3e7
adarshwritescode <adarshpandya0@gmail.com>
2026-08-09 10:52:25 +0530

    fix build

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,9 @@ import "./index.css";
 import App from "./App.tsx";
 
 const root = document.getElementById("root");
+
 if (root) {
-  createRoot(document.getElementById("root")).render(
+  createRoot(root).render(
     <StrictMode>
       <App />
     </StrictMode>,
